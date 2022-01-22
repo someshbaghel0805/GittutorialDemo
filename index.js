61c0433517908ffbdcf166aff4cf4fb3099dab09
Somesh <someshbaghel207@gmail.com>
2022-01-22 19:16:06 +0530

    commit File

---
+++ index.js
@@ -0,0 +1,48 @@
+
+        // num1=(45);
+        // num2=(85);
+        // console.log(num1+num2);
+
+
+        // String
+
+        // var str1="This is a String"
+        // var str2="This is also String"
+        // console.log(num1+num2);
+
+    // Object
+    // var marks ={
+    //     ravi:45,
+    //     Shubham:67,
+    //     Daku:97,
+    //     Mahajan:77
+    // }
+    // var age=31;
+    // if(age>32) {
+    //     console.log("bachhe nhi rahe");
+    // }
+    // else if (age>26){
+    //     console.log("bachhe nhi hai abhe");
+    // }
+    // else if (age>22){
+    //     console.log("bachhe room me hai");
+    // }
+    // else if (age>18){
+    //     console.log("bachhe sorhe hai");
+    // }
+    // else{
+    //     console.log("End of the Ladder");
+    // }
+    // Array
+    var arr=[1,2,3,4,5,6,7];
+    // {
+    //     console.log(arr);
+    // }
+    for(i=0; i<arr.length; i++)
+    {
+        console.log(arr[i]);
+    }
+
+
+
+        
